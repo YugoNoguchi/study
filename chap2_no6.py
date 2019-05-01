76cde027bd25a79854b364a8bb238551f8d5c829
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-05-01 17:09:15 +0900

    check

--- chap2_no6.py
+++ chap2_no6.py
@@ -1,8 +1,13 @@
 def main():
-    text = input("nennreiha?")
-    age = int(text)
+    text = ("年齢は？")
+    age = int (text)
     if age < 20 :
-        print("miseinenn")
+        print("未成年")
+    elif age > 65 :
+        print ("成人")
+    else :
+        print ("高齢者")
+
 
 if __name__ == "__main__":
     main()

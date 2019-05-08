0aa28facd00730aca45a94d9a74f747b22945c22
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-05-08 16:18:28 +0900

    new_ver2

---
+++ chap2no9ver2_2.py
@@ -0,0 +1,24 @@
+def main () :
+    text = input ("年齢は？")
+    if text . isdigit () :
+        age = int (text)
+        if age < 20 :
+            if age >= 6 and age <=15 :
+                print ("義務教育期間中の未成年")
+            elif age <=5 :
+                print ("幼児")
+            else :
+                print ("未成年")
+        elif age < 65 :
+            print ("成人")
+        else :
+            print ("高齢者")
+if __name__ == ("__main__") :
+    main()
+
+"""
+未成年
+(義務教育期間中の未成年)6_15
+成人
+高齢者
+"""

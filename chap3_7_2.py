84f786d45b32c97d0fd9c2b6a7905e606d690912
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-05-23 13:32:37 +0900

    9*9＝

---
+++ chap3_7_2.py
@@ -0,0 +1,9 @@
+def main () :
+    for cnt1 in range ( 1 , 10 ) :
+        for cnt2 in range ( 1 , 10 ) :
+            print ( cnt1 , "×" , cnt2 , "＝" , cnt1 * cnt2 )
+
+
+
+if __name__ == ("__main__"):
+    main ()

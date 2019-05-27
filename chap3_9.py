37d2bb6a220bd34f49f4505339f0f5c2b8554053
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-05-27 23:43:40 +0900

    総当たり戦

---
+++ chap3_9.py
@@ -0,0 +1,7 @@
+def main () :
+    team = [ "A" , "B" , "C" , "D" ,"E" ]
+    for team1 in team :
+        for team2 in team :
+            print ( team1 , " vs " , team2 )
+if __name__ == ("__main__") :
+    main ()

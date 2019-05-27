75b32a586712b1b09d94acfce800a3d46dc80870
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-05-28 00:24:44 +0900

    同じ組み合わせの省略

---
+++ chap3_9_3.py
@@ -0,0 +1,9 @@
+def main () :
+    team = ["A","B","C","D","E"]
+    oops = ["A","B","C","D","E"]
+    for team1 in team :
+        oops . remove ( team1 )
+        for taam2 in oops :
+            print ( team1 , "vs" , team2 )
+if __name__ == ("__main__") :
+    main ()

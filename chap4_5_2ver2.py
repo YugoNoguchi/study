fd118784ac6d0105b3852d2974a99e9df7c2beb0
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-06-01 14:52:40 +0900

    リストの中の辞書、2回目

---
+++ chap4_5_2ver2.py
@@ -0,0 +1,24 @@
+def main () :
+    date = [ { "name" : "Yamamoto" , "bill" : "40000" , "crg" : True } , { "name" : "Yoshida" , "bill" : "25000" , "crg" : True } ]
+    print ( date [ 1 ] [ "name" ] )
+    print ( date [ 1 ] [ "bill" ] )
+
+if __name__ == ( "__main__" ):
+    main ()
+
+
+
+
+
+
+
+"""
+def main () :
+    date = [ { "name" : "Yamamoto" , "bill" : "40000" , "crg" : True } , { "name" : "Yoshida" , "bill" : "25000" , "crg" : True } ]
+    print ( date [ 1 ] , [ "name" ] )
+    print ( date [ 1 ] , [ "bill" ] )
+
+if __name__ == ( "__main__" ):
+    main ()
+
+"""

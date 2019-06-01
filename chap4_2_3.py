c0afcab28ade1239f140f015ddaa60c26e8287cb
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-06-01 12:59:59 +0900

    複数人宛ての定型文”
    ”
    ”
    
    ””

---
+++ chap4_2_3.py
@@ -0,0 +1,29 @@
+def main () :
+    def create_mail ( recv ) :
+        print ( recv , "様" )
+        print ("PT企画の佐藤です。")
+        print ("今月の請求書をおくります。")
+
+    create_mail ("山本")
+    create_mail ("吉田")
+
+if __name__ == ("__main__") :
+    main ()
+
+
+
+
+
+"""
+
+def main () :
+    def create_mial () :
+        print ("PT規画の佐藤です。")
+        print ("今月の請求書をおくります。")
+    create_mial
+
+if __name__ == ("__main__") :
+    main ()
+
+
+"""

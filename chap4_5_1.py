80d868f0db1008b39e6e5344046492e9218fb4e6
Noguchi Yugo <iamnukuma.skater@gmail.com>
2019-06-01 14:26:20 +0900

    辞書

---
+++ chap4_5_1.py
@@ -0,0 +1,8 @@
+def main () :
+    date = { "name" : "Yamamoto" , "bill" : "40000" , "crg" : True }
+    print ( date["name"] )
+    print ( date["bill"] )
+
+
+if __name__ == ("__main__") :
+    main ()
